341f106a631975684f2d0d2f0b13315659ba5fd0
Aaron Boyd <aaron.boyd.org@gmail.com>
2020-03-01 21:33:11 -0800

    made some fixes to the game class

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-
+import sys
 
 def singleton(myClass):
 	instances = {}
@@ -12,14 +12,27 @@ def singleton(myClass):
 @singleton
 class Game:
     
-    __game_states = ["INTRO", "STANDARDTURN", "COMBATTURN"]
+    _game_states = ["intro", "standard_turn", "combat_turn"]
 
     def __init__(self, initial_game_state):
-        self.__game_state = initial_game_state
+        self._game_state = initial_game_state
 
     def set_game_state(self, new_state):
-        if new_state in __game_states:
-            self.__game_state = new_state
+        if new_state.lower() in _game_states:
+            self._game_state = new_state
 
-    def execute_current_state(self):
+    def execute_game_state(self):
+        try:
+            self._game_states[self._game_state]()
+        except NameError:
+            print("Error! Current game state does not exist!")
+            sys.exit()
+
+    def intro(self):
         pass
+
+    def normal_turn(self):
+        pass
+
+    def combat_turn(self):
+        pass 
